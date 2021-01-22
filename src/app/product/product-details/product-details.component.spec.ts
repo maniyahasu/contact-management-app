0ba37b9383c510350574ee2b2d8d11c2dd3b88bb
Hasmukh Maniya <hasumaniya@gmail.com>
2021-01-23 00:03:46 +0530

    Removed comment test commit

--- src/app/product/product-details/product-details.component.spec.ts
+++ src/app/product/product-details/product-details.component.spec.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-// import { ActivatedRoute, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 
